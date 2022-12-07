3c9b6a9270e133bc1f60122c6a025c53ad04610a
Ali200119 <aliit@code.edu.az>
2022-12-08 02:38:06 +0400

    Complete

---
+++ CSharp Intro/CSharp Intro/Program.cs
@@ -0,0 +1,89 @@
+﻿#region Task 1
+
+//Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+//int n = 21;
+
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("Bolunur");
+//}
+
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+#endregion
+
+#region Task 2
+
+//n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+//int n = 12;
+//int m = 22;
+
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    Console.WriteLine(n + m);
+//}
+
+#endregion
+
+#region Task 3
+
+//Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+//int n = 20;
+//int m = 50;
+//int sum = 0;
+
+//for (int i = n; i < m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+#endregion
+
+#region Task 4
+
+//Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+//int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int sum = 0;
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] % 2 == 1)
+//    {
+//        sum += array[i];
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+#endregion
+
+#region Task 5
+
+//Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+//int[] array = { 47, 13, 24, 20, 55, 78, 10, 11 };
+//int count = 0;
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] % 2 == 0)
+//    {
+//        count++;
+//    }
+//}
+
+//Console.WriteLine(count);
+
+#endregion
